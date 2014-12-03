e76835cdc0a75dde9c50578f8787fef77a886dd3
jma17 <jma17@punahou.edu>
2014-12-02 19:38:55 -1000

    made snake move and more.
    My snake can move around and eat apples but he doesn’t get longer.

--- Snake_Game.ino
+++ Snake_Game.ino
@@ -38,69 +38,124 @@
  */
 
 #include <MeggyJrSimple.h>    // Required code, line 1 of 2.
-int xsnake = 0;
-int ysnake = 0;
+int xplayer = 3;              //coordinates of player dot
+int yplayer = 4;
+int marker = 3;
 int xapple = random(8);
 int yapple = random(8);
-boolean gotApple = false;
+int dir = 0;
+
+int i = 0;// posible values are 0,90,180,270
+
+struct Point
+{
+  int x;
+  int y;
+  
+};
+Point p1 = {3,4};
+Point p2 = {4,3};
+Point p3 = {5,4};
+
+Point snakeArray [64] = {p1,p2,p3};
+
+void drawsnake()
+{
+  for(int i=0; i<marker; i++);
+    {
+      DrawPx(snakeArray [i].x, snakeArray [i].y, Red);
+    }
+}    
 
 void setup()                    // run once, when the sketch starts
 {
+  Serial.begin(9600);
   MeggyJrSimpleSetup();      // Required code, line 2 of 2.
 }
 
-void loop()                     // run over and over again
-{
-  
-  struct Point
-  { 
-    int x;
-    int y;// waits for a second
-  };
 
+void loop()                     // run over and over again
+/*
+  Draw player
+  Display slate
+  Delay
+  Check buttons
+  update direction if a button is pressed
+  update player based on direction
+  Correct for wrap
+*/
+
+{
+  Serial.print("X is");
+  Serial.println(xplayer);
+  Serial.print("Y is");
+  Serial.println(yplayer);
+  Serial.println();
+  
+  DrawPx(xplayer,yplayer,Red);
+  DrawPx(xapple,yapple,Green);
+  DisplaySlate();
+  delay(300);
+  ClearSlate();
   CheckButtonsPress();
   if (Button_Right)
-    {         
-      if (xsnake < 7)
-        xsnake++;
-    }
-   
-  if (Button_Left)
-    {         
-      if (xsnake > 0)
-        xsnake--;
-    }
-    
-  if (Button_Up)
-    {         
-      if (ysnake < 7)
-        ysnake++;
-    }
-  if (Button_Down)
-    {         
-      if (ysnake > 0)
-        ysnake--;
-    }
-  if (xapple == xsnake)
-    if (yapple == ysnake)
-      gotApple = true;
-  
-  
-
-  DrawPx(xsnake,ysnake,1);
-  
-  DisplaySlate();
-  delay(1);
-  ClearSlate();
-  
-  DrawPx(xapple,yapple,Green);
-  
-  if (gotApple = true)
+  {
+    dir = 90;
+  }
+  if(dir==0)
     {
-      if(ReadPx(xsnake,ysnake) == Green)
+      yplayer ++;
+    }  
+  if (Button_Left)
+  {
+    dir = 270;
+  }
+  if(dir==90)
+    {
+      xplayer ++;
+    }  
+  if (Button_Up)
+  {
+    dir = 0;
+  }
+  if(dir==180)
+    {
+      yplayer --;
+    }  
+  
+  if (Button_Down)
+  {
+    dir = 180;
+  }
+  if(dir==270)
+    {
+      xplayer --;
+    }  
+  if(xplayer > 7)
+    {
+      xplayer=0;
+    }  
+  if(yplayer > 7)
+    {
+      yplayer=0;
+    }   
+  if(xplayer < 0)
+    {
+      xplayer=7;
+    }  
+  if(yplayer < 0)
+    {
+      yplayer=0;
+    } 
+  if (xapple==xplayer)
+    {
+      if(yapple==yplayer)
         {
-          ClearSlate();         
-          DrawPx(xapple,yapple,Green);
+          xapple = random(8);
+          yapple = random(8);
         }
     }
+    
+ 
+    
 }
